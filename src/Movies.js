f4abd5c8c765fa4f627768cce8b9d52a8ed9dcfb
Mohan Lal Deshmukh <112425010+Mohand41@users.noreply.github.com>
2023-02-14 14:59:41 +0530

    14-2

--- src/Movies.js
+++ src/Movies.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom';
-import { useGlobalContext } from './Context'
+import { useGlobalContext } from './Context';
 
 const Movies = () => {
   const {movie, isLoading} = useGlobalContext();
